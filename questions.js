8746f29b665b4a60f8a53950389d3078fdc310a4
Norman Munge <normanmunge@gmail.com>
2022-08-11 16:54:30 +0300

    Adds challenge 4 on closures

--- questions.js
+++ questions.js
@@ -58,8 +58,8 @@ function outer() {
   return incrementCounter;
 }
 
-const willCounter = outer();
-const jasCounter = outer();
+// const willCounter = outer();
+// const jasCounter = outer();
 
 // Uncomment each of these lines one by one.
 // Before your do, guess what will be logged from each function call.
@@ -80,7 +80,7 @@ function addByX(x) {
 }
 
 // /*** Uncomment these to check your work! ***/
-// const addByTwo = addByX(2);
+const addByTwo = addByX(2);
 // addByTwo(1); // => should return 3
 // addByTwo(2); // => should return 4
 // addByTwo(3); // => should return 5
@@ -92,5 +92,37 @@ function addByX(x) {
 // const addByFour = addByX(4);
 // addByFour(4); // => should return 8
 // addByFour(5); // => should return 9
+
+//------------------------------------------------------------------------------------ //
+
+//----------------------------CHALLENGE 4---------------------------------------------- //
+
+/**
+ * Write a function once that accepts a callback as input and returns a function.
+ * When the returned function is called the first time, it should call the callback and return that output.
+ * If it is called any additional times, instead of calling the callback again it will simply return the output value from the first time it was called.
+ */
+
+function once(callback) {
+  let counter = 0;
+  let value;
+
+  const executeCallback = (y) => {
+    if (counter < 1) {
+      counter++;
+      return (value = callback(y));
+    } else {
+      return value;
+    }
+  };
+
+  return executeCallback;
+}
+
+// /*** Uncomment these to check your work! ***/
+const onceFunc = once(addByTwo);
+console.log(onceFunc(4)); // => should log 6
+console.log(onceFunc(10)); // => should log 6
+console.log(onceFunc(9001)); // => should log 6
 
 //------------------------------------------------------------------------------------ //
