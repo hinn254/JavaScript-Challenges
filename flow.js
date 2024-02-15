9c7b85d9fb09e1b6aebd12e288761462fde23212
Benny H. Otieno <bennyhinnotieno@gmail.com>
2024-02-16 00:44:51 +0300

    Update flow.js

--- flow.js
+++ flow.js
@@ -246,3 +246,28 @@ function disemvowel(string) {
   
 }
 
+
+/* =========== *
+* Challenge 13 *
+* ============ */
+
+function divisibleByFivePairSum(array){
+  // your code here...
+  let indexarr = []
+  for (let i = 0; i < array.length; i++){
+		for (let j=i+1; j < array.length; j++){
+					if ((array[i] + array[j]) % 5 == 0){
+					indexarr.push([i,j])
+			}
+    }
+  }
+  	
+	console.log(indexarr)
+  
+}
+
+
+//Uncomment the lines below to test your function:
+
+console.log(divisibleByFivePairSum([1, 5, 2, 0, 4])); // => [ [ 0, 4 ], [ 1, 3 ] ]
+console.log(divisibleByFivePairSum([13, 22, 8, -3, 12])); // => [[ 0, 1 ], [ 0, 3 ], [ 0, 4 ], [ 1, 2 ], [ 2, 3 ], [ 2, 4 ]]
